79233854dfb39df85e01195b59fcd92ab5d807b2
salma <salmakchaw123@gmail.com>
2024-04-18 20:25:33 +0100

    added route auth for Authentification

--- src/App.js
+++ src/App.js
@@ -1,58 +1,21 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./Components/Home.tsx";
+import Test from "./Components/Test.tsx";
+import Authentification from './Components/Authentification.js';
 import React from "react";
-import * as Components from './Components/Component';
+
 
 function App() {
     const [signIn, toggle] = React.useState(true);
-     return(
-         <Components.Container>
-             <Components.SignUpContainer signinIn={signIn}>
-                 <Components.Form>
-                     <Components.Title>Create Account</Components.Title>
-                     <Components.Input type='text' placeholder='Name' />
-                     <Components.Input type='email' placeholder='Email' />
-                     <Components.Input type='password' placeholder='Password' />
-                     <Components.Button>Sign Up</Components.Button>
-                 </Components.Form>
-             </Components.SignUpContainer>
-
-             <Components.SignInContainer signinIn={signIn}>
-                  <Components.Form>
-                      <Components.Title>Sign in</Components.Title>
-                      <Components.Input type='email' placeholder='Email' />
-                      <Components.Input type='password' placeholder='Password' />
-                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                      <Components.Button>Sigin In</Components.Button>
-                  </Components.Form>
-             </Components.SignInContainer>
-
-             <Components.OverlayContainer signinIn={signIn}>
-                 <Components.Overlay signinIn={signIn}>
-
-                 <Components.LeftOverlayPanel signinIn={signIn}>
-                     <Components.Title>Welcome Back!</Components.Title>
-                     <Components.Paragraph>
-                         To keep connected with us please login 
-                     </Components.Paragraph>
-                     <Components.GhostButton onClick={() => toggle(true)}>
-                         Sign In
-                     </Components.GhostButton>
-                     </Components.LeftOverlayPanel>
-
-                     <Components.RightOverlayPanel signinIn={signIn}>
-                       <Components.Title>Hey, Friend!</Components.Title>
-                       <Components.Paragraph>
-                           Don't have an account ? sign up Now !!!
-                       </Components.Paragraph>
-                           <Components.GhostButton onClick={() => toggle(false)}>
-                               Sigin Up
-                           </Components.GhostButton> 
-                     </Components.RightOverlayPanel>
- 
-                 </Components.Overlay>
-             </Components.OverlayContainer>
-
-         </Components.Container>
-     )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="test" element={<Test/>}></Route>
+                <Route path="auth" element={<Authentification signIn={signIn} toggle={toggle} />}></Route>
+            </Routes>
+        </BrowserRouter>
+        )
 }
-
 export default App;
---
+++ src/Components/Authentification.js
@@ -0,0 +1,56 @@
+import * as Components from './Component.js';
+
+
+function  Authentification({ signIn, toggle }){
+    return(
+        <Components.Container>
+            <Components.SignUpContainer signinIn={signIn}>
+                <Components.Form>
+                    <Components.Title>Create Account</Components.Title>
+                    <Components.Input type='text' placeholder='Name' />
+                    <Components.Input type='email' placeholder='Email' />
+                    <Components.Input type='password' placeholder='Password' />
+                    <Components.Button>Sign Up</Components.Button>
+                </Components.Form>
+            </Components.SignUpContainer>
+
+            <Components.SignInContainer signinIn={signIn}>
+                 <Components.Form>
+                     <Components.Title>Sign in</Components.Title>
+                     <Components.Input type='email' placeholder='Email' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                     <Components.Button>Sigin In</Components.Button>
+                 </Components.Form>
+            </Components.SignInContainer>
+
+            <Components.OverlayContainer signinIn={signIn}>
+                <Components.Overlay signinIn={signIn}>
+
+                <Components.LeftOverlayPanel signinIn={signIn}>
+                    <Components.Title>Welcome Back!</Components.Title>
+                    <Components.Paragraph>
+                        To keep connected with us please login 
+                    </Components.Paragraph>
+                    <Components.GhostButton onClick={() => toggle(true)}>
+                        Sign In
+                    </Components.GhostButton>
+                    </Components.LeftOverlayPanel>
+
+                    <Components.RightOverlayPanel signinIn={signIn}>
+                      <Components.Title>Hey, Friend!</Components.Title>
+                      <Components.Paragraph>
+                          Don't have an account ? sign up Now !!!
+                      </Components.Paragraph>
+                          <Components.GhostButton onClick={() => toggle(false)}>
+                              Sigin Up
+                          </Components.GhostButton> 
+                    </Components.RightOverlayPanel>
+
+                </Components.Overlay>
+            </Components.OverlayContainer>
+
+        </Components.Container>
+    )
+}
+export default Authentification;
