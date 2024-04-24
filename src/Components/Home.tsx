f26b14f11df9f4044a7f333ecdfcd227199e9ed7
chadda <chadha.essid@insat.ucar.tn>
2024-04-24 14:47:09 +0100

    initital commit

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 function  Home(){
     return(
         <>
-            <div>
+            <div >
                 This is the Home page
             </div>
             <Link to="/test" className="btn v">Go to test page</Link>
