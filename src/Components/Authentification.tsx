7fd44ea94a8be6ccc33d6059df5fa8eea4ed36b8
chadda <chadha.essid@insat.ucar.tn>
2024-04-25 11:00:56 +0100

    fetching api endpoint for login with dummy data

--- src/Components/Authentification.tsx
+++ src/Components/Authentification.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from 'react';
 import * as Components from './Component.tsx';
+import Test from "./Test.tsx";
 
 interface AuthentificationProps {
   signIn: boolean;
@@ -7,90 +8,100 @@ interface AuthentificationProps {
 }
 
 const Authentification: FC<AuthentificationProps> = ({ signIn, toggle }) => {
-  const [registrationData, setRegistrationData] = useState(null);
-  const [loginData, setLoginData] = useState(null);
-  const [error, setError] = useState<Error | null>(null);
+    const errorStyle={
+        //Todo:style error messages
+    }
+    const [loginInfo,setLoginInfo] = useState([]);
+    const [error,setError] = useState([]);
+    const loginInputDetails={
+        "email":"laysuuuuuuun@gmail.com",
+        "password":"123456"
+    }
 
-  useEffect(() => {
-    const fetchRegistrationData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/authentication/register');
-        if (!response.ok) {
-          throw new Error('Registration failed');
-        }
-        const jsonData = await response.json();
-        setRegistrationData(jsonData);
-      } catch (error:any) {
-        setError(error);
-      }
-    };
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+            fetch('http://localhost:3000/authentication/login', {
+                method: "POST",
+                body: JSON.stringify(loginInputDetails),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    console.log("data : ", data);
+                    if (data['statusCode'] == 200) {
+                        setLoginInfo(data)
+                        console.log("success info ", loginInfo)
 
-    const fetchLoginData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/authentication/login');
-        if (!response.ok) {
-          throw new Error('Login failed');
-        }
-        const jsonData = await response.json();
-        setLoginData(jsonData);
-      } catch (error:any) {
-        setError(error);
-      }
-    };
+                    } else {
+                        setError(data['message'])
+                        console.log("errors:", data['message'])
 
-    fetchRegistrationData();
-    fetchLoginData();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+                    }
+                });
+    }
   return (
-    <Components.Container>
-      <Components.SignUpContainer signinin={signIn}>
-        <Components.Form>
-          <Components.Title>Create Account</Components.Title>
-          <Components.Input type='text' placeholder='Name' />
-          <Components.Input type='email' placeholder='Email' />
-          <Components.Input type='password' placeholder='Password' />
-          <Components.Button>Sign Up</Components.Button>
-        </Components.Form>
-      </Components.SignUpContainer>
+      <>
 
-      <Components.SignInContainer signinin={signIn}>
-        <Components.Form>
-          <Components.Title>Sign in</Components.Title>
-          <Components.Input type='email' placeholder='Email' />
-          <Components.Input type='password' placeholder='Password' />
-          <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-          <Components.Button>Sigin In</Components.Button>
-        </Components.Form>
-      </Components.SignInContainer>
+          <Components.Container>
+              <Components.SignUpContainer signinin={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Account</Components.Title>
+                      <Components.Input type='text' placeholder='Name'/>
+                      <Components.Input type='email' placeholder='Email'/>
+                      <Components.Input type='password' placeholder='Password'/>
+                      <Components.Button>Sign Up</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
 
-      <Components.OverlayContainer signinin={signIn}>
-        <Components.Overlay signinin={signIn}>
-          <Components.LeftOverlayPanel signinin={signIn}>
-            <Components.Title>Welcome Back!</Components.Title>
-            <Components.Paragraph>
-              To keep connected with us please login
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(true)}>
-              Sign In
-            </Components.GhostButton>
-          </Components.LeftOverlayPanel>
+              <Components.SignInContainer signinin={signIn}>
+                  <Components.Form onSubmit={handleFormSubmit}>
+                      <Components.Title>Sign in</Components.Title>
+                      <Components.Input type='email' placeholder='Email'/>
+                      <Components.Input type='password' placeholder='Password'/>
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                      <Components.Button>Sigin In</Components.Button>
+                      {error &&
+                      <Components.Paragraph>
+                                  <ul style={errorStyle}>
+                                      {error.map((err, index) => (
+                                          <li key={index}>{err}</li>
+                                      ))}
+                                  </ul>
 
-          <Components.RightOverlayPanel signinin={signIn}>
-            <Components.Title>Hey, Friend!</Components.Title>
-            <Components.Paragraph>
-              Don't have an account ? sign up Now !!!
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(false)}>
-              Sigin Up
-            </Components.GhostButton>
-          </Components.RightOverlayPanel>
-        </Components.Overlay>
-      </Components.OverlayContainer>
-    </Components.Container>
+                      </Components.Paragraph>
+                      }
+                  </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinin={signIn}>
+                  <Components.Overlay signinin={signIn}>
+                      <Components.LeftOverlayPanel signinin={signIn}>
+                          <Components.Title>Welcome Back!</Components.Title>
+                          <Components.Paragraph>
+                              To keep connected with us please login
+                          </Components.Paragraph>
+                          <Components.GhostButton onClick={() => toggle(true)}>
+                              Sign In
+                          </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinin={signIn}>
+                          <Components.Title>Hey, Friend!</Components.Title>
+                          <Components.Paragraph>
+                              Don't have an account ? sign up Now !!!
+                          </Components.Paragraph>
+                          <Components.GhostButton onClick={() => toggle(false)}>
+                              Sigin Up
+                          </Components.GhostButton>
+                      </Components.RightOverlayPanel>
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+          </Components.Container>
+      </>
   );
 };
 
