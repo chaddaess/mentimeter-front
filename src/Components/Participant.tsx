fb3b022014518777338b5ed758fcaef9ad28ae11
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-02 16:10:54 +0100

    Text align

--- src/Components/Participant.tsx
+++ src/Components/Participant.tsx
@@ -1,7 +1,7 @@
 const Participant = ({data}) => (<div className="participant">
         <img src={data.avatar} alt={`${data.name}'s avatar`}
              style={{width: 50, height: 50, borderRadius: '50%', marginRight: 10}}/>
-        <div>
+        <div className="stats">
             <h4>{data.name}</h4>
             <p>Score: {data.score}</p>
         </div>
