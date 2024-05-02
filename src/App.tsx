d2810969330014ab5a7e660ee396f49eee81a04a
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-02 14:58:25 +0100

    Added leaderboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from "./Components/Home.tsx";
-import Test from "./Components/Test.tsx";
 import Authentification from './Components/Authentification.tsx';
 import LogoutComponent from "./Components/LogoutComponent.tsx"
 import React from "react";
-
 import './App.css'
 import PrivateRoutes from "./Components/PrivateRoutes.tsx";
 import BuildQuiz from "./Components/BuildQuiz.tsx";
+import Leaderboard from "./Components/Leaderboard.tsx";
 
 function App() {
     const [signIn, toggle] = React.useState(true);
-  return (
-    <BrowserRouter>
+    return (<BrowserRouter>
         <Routes>
             <Route element={<PrivateRoutes/>}>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path='/build' element={<BuildQuiz/>}></Route>
-            <Route path="test" element={<Test/>}></Route>
             </Route>
             <Route path="/logout"
-                  element={
-                    <LogoutComponent/>
-                  }
+                   element={<LogoutComponent/>}
             >
             </Route>
-            <Route path="authentication" element={
-                <Authentification
-                    signIn={signIn}
-                    toggle={toggle}
-                />
-            }>
+            <Route path="authentication" element={<Authentification
+                signIn={signIn}
+                toggle={toggle}
+            />}>
             </Route>
+            <Route path="leaderboard" element={<Leaderboard/>}></Route>
+
         </Routes>
 
-    </BrowserRouter>
-    )
+    </BrowserRouter>)
 }
+
 export default App;
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,33 +3,25 @@ import MainHomeBox from "./MainHomeBox.tsx";
 
 function Home() {
     const containerStyle = {
-        display: 'flex',
-        height: '200vh', // Adjust as needed
-        padding: 0,
-        margin: 0,
-        width: "98%",
+        display: 'flex', height: '200vh', // Adjust as needed
+        padding: 0, margin: 0, width: "98%",
     };
 
     const mainContentStyle = {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        paddingLeft: '23%', // Adjust based on the width of the sidebar
+        flex: 1, display: 'flex', flexDirection: 'column', paddingLeft: '23%', // Adjust based on the width of the sidebar
     };
-    const userinfo=JSON.parse(localStorage.getItem('loginInfo'))
+    const userinfo = JSON.parse(localStorage.getItem('loginInfo'))
     console.log(userinfo)
-    const   username=userinfo!=null?userinfo['username']:"stranger"
+    const username = userinfo != null ? userinfo['username'] : "stranger"
 
-    return (
-        <>
+    return (<>
             <div className="container" style={containerStyle}>
-                <SideBar />
+                <SideBar/>
                 <div className="main-content" style={mainContentStyle}>
                     <MainHomeBox name={username}/>
                 </div>
             </div>
-        </>
-    );
+        </>);
 }
 
 export default Home;
