828860208d8cf2a083f91e73aaaeee9c407955de
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-10 17:51:01 +0100

    UI Fixes

--- src/Components/Authentification.tsx
+++ src/Components/Authentification.tsx
@@ -43,68 +43,67 @@ const Authentification: FC<AuthentificationProps> = ({signIn, toggle}) => {
             });
     }
     return (<>
-        {!localStorage.getItem('loginInfo') ? <div>
-                <Components.Container>
-                    <Components.SignUpContainer signinin={signIn}>
-                        <Components.Form onSubmit={(event) => handleFormSubmit(event, 'register')}>
-                            <Components.Title>Create Account</Components.Title>
-                            <Components.Input name="email" type='email' placeholder='Email'
-                                              onChange={handleInputChange}/>
-                            <Components.Input name="password" type='password' placeholder='Password'
-                                              onChange={handleInputChange}/>
-                            <Components.Button>Sign Up</Components.Button>
-                            {error && <Components.Paragraph>
-                                <ul style={Components.errorStyle}>
-                                    {error[0]}
-                                </ul>
-                            </Components.Paragraph>}
-                        </Components.Form>
+        {!localStorage.getItem('loginInfo') ? <div className="centered_div">
+            <Components.Container>
+                <Components.SignUpContainer signinin={signIn}>
+                    <Components.Form onSubmit={(event) => handleFormSubmit(event, 'register')}>
+                        <Components.Title>Create Account</Components.Title>
+                        <Components.Input name="email" type='email' placeholder='Email'
+                                          onChange={handleInputChange}/>
+                        <Components.Input name="password" type='password' placeholder='Password'
+                                          onChange={handleInputChange}/>
+                        <Components.Button>Sign Up</Components.Button>
+                        {error && <Components.Paragraph>
+                            <ul style={Components.errorStyle}>
+                                {error[0]}
+                            </ul>
+                        </Components.Paragraph>}
+                    </Components.Form>
 
-                    </Components.SignUpContainer>
+                </Components.SignUpContainer>
 
-                    <Components.SignInContainer signinin={signIn}>
-                        <Components.Form onSubmit={(event) => handleFormSubmit(event, 'login')}>
-                            <Components.Title>Sign in</Components.Title>
-                            <Components.Input name="email" type='email' placeholder='Email'
-                                              onChange={handleInputChange}/>
-                            <Components.Input name="password" type='password' placeholder='Password'
-                                              onChange={handleInputChange}/>
-                            {/*<Components.Anchor href='#'>Forgot your password?</Components.Anchor>*/}
-                            <Components.Button>Sigin In</Components.Button>
-                            {error && <Components.Paragraph>
-                                <ul style={Components.errorStyle}>
-                                    {error[0]}
-                                </ul>
-                            </Components.Paragraph>}
-                        </Components.Form>
+                <Components.SignInContainer signinin={signIn}>
+                    <Components.Form onSubmit={(event) => handleFormSubmit(event, 'login')}>
+                        <Components.Title>Sign in</Components.Title>
+                        <Components.Input name="email" type='email' placeholder='Email'
+                                          onChange={handleInputChange}/>
+                        <Components.Input name="password" type='password' placeholder='Password'
+                                          onChange={handleInputChange}/>
+                        {/*<Components.Anchor href='#'>Forgot your password?</Components.Anchor>*/}
+                        <Components.Button>Sigin In</Components.Button>
+                        {error && <Components.Paragraph>
+                            <ul style={Components.errorStyle}>
+                                {error[0]}
+                            </ul>
+                        </Components.Paragraph>}
+                    </Components.Form>
 
-                    </Components.SignInContainer>
-                    <Components.OverlayContainer signinin={signIn}>
-                        <Components.Overlay signinin={signIn}>
-                            <Components.LeftOverlayPanel signinin={signIn}>
-                                <Components.Title>Welcome Back!</Components.Title>
-                                <Components.Paragraph>
-                                    To keep connected with us please login
-                                </Components.Paragraph>
-                                <Components.GhostButton onClick={() => toggle(true)}>
-                                    Sign In
-                                </Components.GhostButton>
-                            </Components.LeftOverlayPanel>
+                </Components.SignInContainer>
+                <Components.OverlayContainer signinin={signIn}>
+                    <Components.Overlay signinin={signIn}>
+                        <Components.LeftOverlayPanel signinin={signIn}>
+                            <Components.Title>Welcome Back!</Components.Title>
+                            <Components.Paragraph>
+                                To keep connected with us please login
+                            </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(true)}>
+                                Sign In
+                            </Components.GhostButton>
+                        </Components.LeftOverlayPanel>
 
-                            <Components.RightOverlayPanel signinin={signIn}>
-                                <Components.Title>Hey, Friend!</Components.Title>
-                                <Components.Paragraph>
-                                    Don't have an account ? sign up Now !!!
-                                </Components.Paragraph>
-                                <Components.GhostButton onClick={() => toggle(false)}>
-                                    Sigin Up
-                                </Components.GhostButton>
-                            </Components.RightOverlayPanel>
-                        </Components.Overlay>
-                    </Components.OverlayContainer>
-                </Components.Container>
-            </div>
-            : <Navigate to='/home'/>}
+                        <Components.RightOverlayPanel signinin={signIn}>
+                            <Components.Title>Hey, Friend!</Components.Title>
+                            <Components.Paragraph>
+                                Don't have an account ? sign up Now !!!
+                            </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton>
+                        </Components.RightOverlayPanel>
+                    </Components.Overlay>
+                </Components.OverlayContainer>
+            </Components.Container>
+        </div> : <Navigate to='/home'/>}
     </>);
 };
 
