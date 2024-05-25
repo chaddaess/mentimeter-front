2777d8f640e4b316168bb764d4afe814d9b1cc73
chadda <chadha.essid@insat.ucar.tn>
2024-05-25 09:19:26 +0100

    add life to welcomepage

--- src/Components/WelcomePage.tsx
+++ src/Components/WelcomePage.tsx
@@ -1,29 +1,32 @@
 import Navbar from "./Navbar.tsx";
 import Popup from "reactjs-popup";
-import {FaRegCircleXmark} from "react-icons/fa6";
-import {CSSProperties, useState} from "react";
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { CSSProperties, useEffect, useState } from "react";
 import EnterQuizCodeForm from "./EnterQuizCodeForm.tsx";
+import Typewriter from 'typewriter-effect';
 
-function WelcomePage(){
+function WelcomePage() {
     const [open, setOpen] = useState(false);
+    const [fontLoaded, setFontLoaded] = useState(false);
+
     const closeModal = () => setOpen(false);
-    const modalStyle:CSSProperties={
-        width:"60%",
+
+    const modalStyle: CSSProperties = {
+        width: "60%",
         height: "auto",
-        backgroundColor:"#F6F5F2",
+        backgroundColor: "#F6F5F2",
         borderRadius: "12px",
         position: "fixed",
         top: "50%",
-        left: "56%",
+        left: "50%",
         transform: "translate(-50%, -50%)",
         zIndex: 9999,
         display: "flex",
-        flexDirection:"column",
-        padding:" 0.5em 2em"
+        flexDirection: "column",
+        padding: "0.5em 2em"
+    };
 
-
-    }
-    const overlayStyle:CSSProperties = {
+    const overlayStyle: CSSProperties = {
         position: "fixed",
         top: 0,
         left: 0,
@@ -32,9 +35,44 @@ function WelcomePage(){
         backdropFilter: "blur(6px)", // Apply a blur effect
         zIndex: 0, // Ensure the overlay is below the modal
     };
-    return(
+
+    const containerStyle: CSSProperties = {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: "1em",
+    };
+
+    const typewriterContainerStyle: CSSProperties = {
+        minWidth: "200px",
+        marginRight: "1em",
+        fontSize: "3rem",
+        width: "60%",
+        color: "#6C0345",
+    };
+
+    const sideImageStyle: CSSProperties = {
+        maxWidth: "50%",
+        height: "auto"
+    };
+
+    const phrases = [
+        'Are You a Quiz Whiz?',
+        "Wanna  Be Everyone's <i>favourite</i> teacher?",
+        'Your quiz session on another <span style="color: #cd7f32">level!</span>',
+        'Fun, Fast, and Full of Facts!',
+        'Join the Fun – Start a Quiz <span style="color: #cd7f32">Now!</span>'
+    ];
+
+    useEffect(() => {
+        document.fonts.ready.then(() => {
+            setFontLoaded(true);
+        });
+    }, []);
+
+    return (
         <>
-            <Navbar open={open} setOpen={setOpen}/>
+            <Navbar open={open} setOpen={setOpen} />
             {open && <div style={overlayStyle} onClick={closeModal}></div>}
             <Popup
                 open={open}
@@ -42,12 +80,28 @@ function WelcomePage(){
                 onClose={closeModal} >
                 <div className="modal" style={modalStyle}>
                     <a className="close" onClick={closeModal}>
-                        <FaRegCircleXmark  style={{height:"1.5em",width:"1.5em",alignSelf:"flex-end"}}/>
+                        <FaRegCircleXmark style={{ height: "1.5em", width: "1.5em", alignSelf: "flex-end" }} />
                     </a>
-                    <EnterQuizCodeForm/>
+                    <EnterQuizCodeForm />
                 </div>
             </Popup>
+            <div className="welcome-container" style={containerStyle}>
+                {fontLoaded && (
+                    <div style={typewriterContainerStyle}>
+                        <Typewriter
+                            options={{
+                                strings: phrases,
+                                autoStart: true,
+                                loop: true,
+                                delay: 75 // Small delay before starting the typewriter effect
+                            }}
+                        />
+                    </div>
+                )}
+                <img src="/assets/welcome.webp" style={sideImageStyle} />
+            </div>
         </>
-    )
+    );
 }
-export default WelcomePage
+
+export default WelcomePage;
