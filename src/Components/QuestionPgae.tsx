7a7d10304fd382310d1ba7b5cf8deb52e52c5334
salma <salmakchaw123@gmail.com>
2024-05-25 23:43:09 +0100

    no changes

--- src/Components/QuestionPgae.tsx
+++ src/Components/QuestionPgae.tsx
@@ -5,49 +5,44 @@ import io from "socket.io-client";
 
 export default function QuestionPage(){
 
-    const quizId="fff2c4b1-c688-482e-af3c-9de4e77f791c";
+    const quizCode="0db40c19-b7d2-482d-99f7-ecd5d556de96";
     const [questions, setQuestions] = useState([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        // Connect to the WebSocket server
-        const socketConnection = io("http://localhost:3001");
-    
-        socketConnection.on("connect", () => {
+      // Connect to the WebSocket server
+      const socketConnection = io("http://localhost:3001");
+
+      socketConnection.on("connect", () => {
           console.log("Connected to WebSocket server");
-          socketConnection.emit("getQuestion", { quizCode: quizId, questionNumber: currentQuestionIndex });
-        });
-        
-        socketConnection.on("question", (question) => {
-          console.log("here");
-          console.log("Received question:", question);
+          socketConnection.emit("getQuestion", { quizCode: quizCode, questionNumber: currentQuestionIndex });
+      });
+
+      socketConnection.on("question", (question) => {
+          console.log("Received question", question);
           setQuestions((prevQuestions) => [...prevQuestions, question]);
-        });
-        
-        setSocket(socketConnection);
-    
-        return () => {
+      });
+
+      setSocket(socketConnection);
+
+      return () => {
           socketConnection.disconnect();
-        };
-      }, [currentQuestionIndex]);
-    
-      
-      const nextQuestion = () => {
-        const nextIndex = currentQuestionIndex + 1;
-        setCurrentQuestionIndex(nextIndex);
-        socket.emit("getQuestion", { quizCode: quizId, questionNumber: nextIndex });
       };
-    
-      let QuestionText = "";
-      let QuestionOptions = [];
-    
-      if (questions.length > 0) {
-        const currentQuestion = questions[currentQuestionIndex];
-        console.log("currentQuestion", currentQuestion);
-        QuestionText = currentQuestion?.text || "";
-        QuestionOptions = currentQuestion?.options || [];
+    }, []);
+
+  useEffect(() => {
+      if (socket) {
+          socket.emit("getQuestion", { quizCode: quizCode, questionNumber: currentQuestionIndex });
       }
+  }, [currentQuestionIndex, socket]);
+
+  const nextQuestion = () => {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const currentQuestion = questions[currentQuestionIndex] || {};
+  const { question: QuestionText = "", options: QuestionOptions = [] } = currentQuestion;
 
     const Container = styled.div`
     background-color: #fff;
@@ -90,41 +85,41 @@ export default function QuestionPage(){
         fontSize: '1.5em'
     }
     return(
-        <>
+      <>
       <Container>
-        <div>
-          <h2 style={{ marginTop: "8%", marginBottom: "8%" }}>
-            <span style={{ color: "#6C0345", display: "inline-flex", alignItems: "center" }}>
-              <p style={{ fontSize: "7px", margin: 0 }}>
-                <Logo />
-              </p>
-              Quiz
-              <span style={{ color: "#DC6B19" }}>Up</span>
-            </span>
-          </h2>
-          <form>
-            <div>
-              <p style={questionStyle}>{QuestionText}</p>
-            </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <div style={{ marginTop: "1em" }}>
-                <div>
-                  {QuestionOptions.map((option, index) => (
-                    <div key={index} style={{ ...optionStyle, borderColor: borderColors[index] }}>
-                      <input type="radio" id={`option${index + 1}`} name="answer" value={`option${index + 1}`} />
-                      <label htmlFor={`option${index + 1}`}>{option.label}</label>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <button style={buttonStyle} type="button" onClick={nextQuestion}>
-              Next Question
-            </button>
-          </form>
-        </div>
+          <div>
+              <h2 style={{ marginTop: "8%", marginBottom: "8%" }}>
+                  <span style={{ color: "#6C0345", display: "inline-flex", alignItems: "center" }}>
+                      <p style={{ fontSize: "7px", margin: 0 }}>
+                          <Logo />
+                      </p>
+                      Quiz
+                      <span style={{ color: "#DC6B19" }}>Up</span>
+                  </span>
+              </h2>
+              <form>
+                  <div>
+                      <p style={questionStyle}>{QuestionText}</p>
+                  </div>
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                      <div style={{ marginTop: "1em" }}>
+                          <div>
+                              {QuestionOptions.map((option, index) => (
+                                  <div key={index} style={{ ...optionStyle, borderColor: borderColors[index] }}>
+                                      <input type="radio" id={`option${index + 1}`} name="answer" value={`option${index + 1}`} />
+                                      <label htmlFor={`option${index + 1}`}>{option}</label>
+                                  </div>
+                              ))}
+                          </div>
+                      </div>
+                  </div>
+                  <button style={buttonStyle} type="button" onClick={nextQuestion}>
+                      Next Question
+                  </button>
+              </form>
+          </div>
       </Container>
-    </>
+  </>
     )
 
 }
