295ea4f5defd05a7fe69984d3d4c2334b30f46bc
salma <salmakchaw123@gmail.com>
2024-05-25 15:28:33 +0100

    changed to use websocket still doesnt work

--- src/Components/QuestionPgae.tsx
+++ src/Components/QuestionPgae.tsx
@@ -1,36 +1,53 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
+import io from "socket.io-client";
 
 export default function QuestionPage(){
 
     const quizId="fff2c4b1-c688-482e-af3c-9de4e77f791c";
     const [questions, setQuestions] = useState([]);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [socket, setSocket] = useState(null);
+
     useEffect(() => {
-        fetchQuestions();
-    }, []);
-
-    const fetchQuestions = async () => {
-        try {
-            const response = await fetch(`http://localhost:3000/quizzes/${quizId}`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch questions');
-            }
-            const data = await response.json();
-            setQuestions(data.questions);
-        } catch (error) {
-            console.error("Error fetching questions:", error);
-        }
-    };
-
-    let QuestionText = '';
-    let QuestionOptions = [];
-
-    if (questions.length > 0) {
-        const firstQuestion = questions[0];
-        QuestionText = firstQuestion.text;
-        QuestionOptions = firstQuestion.options;
-    }
+        // Connect to the WebSocket server
+        const socketConnection = io("http://localhost:3001");
+    
+        socketConnection.on("connect", () => {
+          console.log("Connected to WebSocket server");
+          socketConnection.emit("getQuestion", { quizCode: quizId, questionNumber: currentQuestionIndex });
+        });
+        
+        socketConnection.on("question", (question) => {
+          console.log("here");
+          console.log("Received question:", question);
+          setQuestions((prevQuestions) => [...prevQuestions, question]);
+        });
+        
+        setSocket(socketConnection);
+    
+        return () => {
+          socketConnection.disconnect();
+        };
+      }, [currentQuestionIndex]);
+    
+      
+      const nextQuestion = () => {
+        const nextIndex = currentQuestionIndex + 1;
+        setCurrentQuestionIndex(nextIndex);
+        socket.emit("getQuestion", { quizCode: quizId, questionNumber: nextIndex });
+      };
+    
+      let QuestionText = "";
+      let QuestionOptions = [];
+    
+      if (questions.length > 0) {
+        const currentQuestion = questions[currentQuestionIndex];
+        console.log("currentQuestion", currentQuestion);
+        QuestionText = currentQuestion?.text || "";
+        QuestionOptions = currentQuestion?.options || [];
+      }
 
     const Container = styled.div`
     background-color: #fff;
@@ -74,37 +91,40 @@ export default function QuestionPage(){
     }
     return(
         <>
-            <Container >
+      <Container>
+        <div>
+          <h2 style={{ marginTop: "8%", marginBottom: "8%" }}>
+            <span style={{ color: "#6C0345", display: "inline-flex", alignItems: "center" }}>
+              <p style={{ fontSize: "7px", margin: 0 }}>
+                <Logo />
+              </p>
+              Quiz
+              <span style={{ color: "#DC6B19" }}>Up</span>
+            </span>
+          </h2>
+          <form>
+            <div>
+              <p style={questionStyle}>{QuestionText}</p>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div style={{ marginTop: "1em" }}>
                 <div>
-                    <h2 style={{marginTop:'8%',marginBottom:'8%'}}>
-                        <span style={{color:"#6C0345",display: "inline-flex", alignItems: "center"}}>
-                            <p style={{fontSize:'7px',margin:0 }}><Logo/></p>
-                            Quiz
-                            <span style={{color:"#DC6B19"}}>Up</span>
-                        </span>
-                        
-                    </h2>
-                    <form >          
-                        <div>
-                            <p style={questionStyle}>{QuestionText}</p>
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <div style={{ marginTop: '1em' }}>
-                        <div>
-                            {QuestionOptions.map((option, index) => (
-                                <div key={index} style={{ ...optionStyle, borderColor: borderColors[index] }}>
-                                    <input type="radio" id={`option${index + 1}`} name="answer" value={`option${index + 1}`} />
-                                    <label htmlFor={`option${index + 1}`}>{option.label}</label>
-                                </div>
-                            ))}
-                        </div>
-                        </div>
-                        </div>
-                        <button style={buttonStyle} type="submit">Submit Answer</button>      
-                    </form>
+                  {QuestionOptions.map((option, index) => (
+                    <div key={index} style={{ ...optionStyle, borderColor: borderColors[index] }}>
+                      <input type="radio" id={`option${index + 1}`} name="answer" value={`option${index + 1}`} />
+                      <label htmlFor={`option${index + 1}`}>{option.label}</label>
+                    </div>
+                  ))}
                 </div>
-            </Container>
-        </>
+              </div>
+            </div>
+            <button style={buttonStyle} type="button" onClick={nextQuestion}>
+              Next Question
+            </button>
+          </form>
+        </div>
+      </Container>
+    </>
     )
 
 }
