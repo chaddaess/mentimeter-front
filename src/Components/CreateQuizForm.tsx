944258bc878ed2e4afeb05e9df1a66ae9048ce15
AbdelhakGhribi <abdelhak.ghribi@insat.ucar.tn>
2024-05-25 15:08:12 +0100

    Building quiz form

--- src/Components/CreateQuizForm.tsx
+++ src/Components/CreateQuizForm.tsx
@@ -1,37 +1,18 @@
-import {CSSProperties, useState} from "react";
-import {randomQuizName} from '../utils/quizname-generator.ts'
-import {useNavigate} from "react-router";
+import { CSSProperties, useState } from "react";
+import { randomQuizName } from '../utils/quizname-generator.ts';
+import { useNavigate } from "react-router";
 
 export default function CreateQuizForm() {
     const [formData, setFormData] = useState({
         name: ''
     });
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        setFormData({...formData, [name]: value});
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
     };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(formData);
-        //Todo:put the correct endpoint to create a quiz here
-        fetch(`http://localhost:3000/quiz/create`, {
-            method: "POST", body: JSON.stringify(formData), headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        }).then((res) => {
-            return res.json();
-        }).then((data) => {
-            console.log(data)
-            navigate('/build')
-        })
 
-        //reset
-        setFormData({
-            name: '',
-        });
-    };
     const formStyle: CSSProperties = {
         display: "flex", flexDirection: "column", justifyContent: "flex-start"
     };
@@ -40,22 +21,25 @@ export default function CreateQuizForm() {
     }
     const inputStyle = {
         width: "100%", height: "3em", borderRadius: "50px", border: "none", paddingLeft: "1em"
-    }
-    return (<>
-        <h3>Let's give your new quiz a name </h3>
-        <form onSubmit={handleSubmit} style={formStyle}>
-            <div>
-                <input
-                    style={inputStyle}
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    placeholder={randomQuizName}
-                />
-            </div>
-            <button style={buttonStyle}>Let's Go !</button>
-        </form>
-    </>)
+    };
+
+    return (
+        <>
+            <h3>Let's give your new quiz a name </h3>
+            <form onSubmit={(event) => { event.preventDefault(); navigate('/build', { state: { quizName: formData.name } }) }} style={formStyle}>
+                <div>
+                    <input
+                        style={inputStyle}
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        placeholder={randomQuizName}
+                    />
+                </div>
+                <button style={buttonStyle}>Let's Go !</button>
+            </form>
+        </>
+    )
 }
